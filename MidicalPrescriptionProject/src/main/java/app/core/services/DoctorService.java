c76084c74e0ad4877d7f6ea96447b4bdcc1e17e4
nur haj <nor.alden@gmail.com>
2021-07-26 23:41:35 +0300

    bbbbb

---
+++ MidicalPrescriptionProject/src/main/java/app/core/services/DoctorService.java
@@ -0,0 +1,15 @@
+package app.core.services;
+
+import javax.transaction.Transactional;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@Scope("prototype")
+public class DoctorService {
+	
+	
+
+}
