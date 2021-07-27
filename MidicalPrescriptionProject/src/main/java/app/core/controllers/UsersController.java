fd9741c6adea12997ba8859bd1aedbda181899ed
nur haj <nor.alden@gmail.com>
2021-07-28 00:37:01 +0300

    ccccc

--- MidicalPrescriptionProject/src/main/java/app/core/controllers/UsersController.java
+++ MidicalPrescriptionProject/src/main/java/app/core/controllers/UsersController.java
@@ -1,17 +1,14 @@
 package app.core.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import app.core.services.UsersService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
 
-	@Autowired
-	private UsersService usersService;
+//	@Autowired
+//	private UsersService usersService;
 
 //	@PostMapping("/register")
 //	public User registerUser(@RequestBody User user, @RequestParam(required = false) Roles role,
